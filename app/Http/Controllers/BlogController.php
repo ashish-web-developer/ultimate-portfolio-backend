6a6ffacd1fe7f85ac9245a1b6af8b16d0661fb75
Ashish <78073173+ashish-web-developer@users.noreply.github.com>
2022-10-09 13:38:07 +0530

    added blog route

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use App\Models\Blog;
+use Illuminate\Support\Facades\Storage;
+
+class BlogController extends Controller
+{
+
+    public function AddBlog(Request $request, Response $response){
+        $blog = new Blog();
+        $blog->blogs = json_encode($request->collect());
+        $blog->save();
+        return response()->json(["message"=>"blog have got created successfully"]);
+    }
+    //
+    public function getBlog(Response $response){
+        $blog = Blog::all()->toArray();
+        return response()->json($blog);
+    }
+
+    public function uploadImage(Request $request){
+        $file = $request->file("image");
+        $name = $file->hashName();
+        Storage::put("public",$file);
+        return response()->json([
+            "success"=>1,
+                "file"=>[
+                    "url"=>"http://localhost:8000/storage/$name"
+                ]
+            ]);
+    }
+}
