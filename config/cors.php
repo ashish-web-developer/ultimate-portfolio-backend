ede27b07967f60819e4e546ba50b81b945525db8
Ashish <78073173+ashish-web-developer@users.noreply.github.com>
2022-10-16 17:37:56 +0530

    updated cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        "http://localhost:3000",
+        "https://ultimate-portfolio-beta.vercel.app",
+        "http://ultimate-portfolio-beta.vercel.app"
+    ],
 
     'allowed_origins_patterns' => [],
 
