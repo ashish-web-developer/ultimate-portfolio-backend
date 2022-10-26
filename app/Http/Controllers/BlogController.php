90d9c8b6ec3639e3b044f9815af17858e0a4c15a
Ashish Prajapati <beingsocasual@gmail.com>
2022-10-27 00:04:52 +0530

    added auth to blogController

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,39 +8,55 @@ use Illuminate\Support\Facades\Log;
 use App\Models\Blog;
 use GrahamCampbell\ResultType\Success;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class BlogController extends Controller
 {
 
     public function AddBlog(Request $request, Response $response){
-        $Validated = $request->validate([
-            "title"=>"required",
+        $Validated = Validator::make($request->all(),[
+            "title"=>"required|string",
             "data"=>"required",
-            "status"=>"required",
-            "featured_image"=>"required"
+            "status"=>"required|boolean",
+            "featured_image"=>"required|string"
         ]);
         if($Validated){
             if($request->id){
                 $blog = Blog::find($request->id);
                 Log::info($blog);
                 $blog->blogs = json_encode(collect($request->data));
-                $blog->title=$request->title;
-                $blog->status=$request->status;
+                $blog->title = $request->title;
+                $blog->status = $request->status;
                 $blog["featured image"] = $request->featured_image;
+                $blog["user"] = $request->user->name;
+                $blog["email"] = $request->user->email;
                 $blog->save();
                 return response()->json(["message"=>"Blog have got updated successfully"]);
 
             }else{
+                $user = $request->user()->toArray();
                 $blog = new Blog();
                 $blog->blogs = json_encode(collect($request->data));
                 $blog->title=$request->title;
                 $blog->status=$request->status;
                 $blog["featured image"] = $request->featured_image;
+                $blog["user"] = $user["name"];
+                $blog["email"] = $user["email"];
                 $blog->save();
-                return response()->json(["message"=>"Blog have got created successfully"]);
+                return response()->json([
+                    "success"=>true,
+                    "message"=>"Blog got created",
+                    "blog"=> $blog
+                ]);
             }
+        }else{
+            return response()->json([
+                "success"=>false,
+                "message"=>"Blog not created",
+                "error"=>$Validated->errors()
+            ]);
         }
-        return response()->json(["message",["param is missing"]],Response::HTTP_BAD_REQUEST);
     }
     //
     public function getBlog(Request $request, Response $response){
@@ -71,16 +87,36 @@ class BlogController extends Controller
             ]);
     }
     public function featuredImageUpload(Request $request){
-        $file = $request->file("image");
-        $name = $file->hashName();
-        Storage::put("public/featured-image",$file);
-        $file_url = asset("/storage/featured-image/$name");
-        Log::info($file_url);
-        return response()->json([
-            "success"=>1,
-            "file"=>[
-                "url"=>$file_url
-            ]
+        Log::info(Auth::user());
+        $validateFile = Validator::make($request->all(),[
+            "image"=> "required|image|mimes:jpeg,png,jpg"
         ]);
+        if($validateFile){
+            try{
+                $file = $request->file("image");
+                $name = $file->hashName();
+                Storage::put("public/featured-image",$file);
+                $file_url = asset("/storage/featured-image/$name");
+                return response()->json([
+                    "success"=>1,
+                    "message"=>"File Uploaded Successfully",
+                    "file"=>[
+                        "url"=>$file_url
+                    ]
+                ],200);
+            }catch(\Throwable $th){
+                return response()->json([
+                    "success"=>false,
+                    "message"=>"File Not Uploaded",
+                    "error"=>$th->getMessage()
+                ],500);
+            }
+        }else{
+            return response()->json([
+                "success"=>false,
+                "message"=>"File Not Uploaded",
+                "error"=>$validateFile->errors()
+            ],401);
+        }
     }
 }
