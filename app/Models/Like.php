4b3c54c7bfee080fef7c6c28729419c24efcd285
Ashish Prajapati <beingsocasual@gmail.com>
2023-03-29 10:32:22 +0530

    [comment functionality] migration

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Comment;
 
 class Like extends Model
 {
     use HasFactory;
+    protected $fillable = ["user_id","blog_id","comment_id","like"];
 }
